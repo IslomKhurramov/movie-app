c2321657bb5ead871636dcbef1597a3a696ee8f0
IslomKhurramov <islom.khurramov@gmail.com>
2023-11-30 05:52:21 +0900

    feat:BRR native wind

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./src/styles/global.css";
+import Home from "./src/pages/home";
+
+export default function App() {
+  return <Home />;
+}
